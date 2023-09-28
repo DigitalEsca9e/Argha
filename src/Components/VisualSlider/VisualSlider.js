b56f704a88936c28cf91a44d9d409e44b9397e86
Argha <arghaghoshd@gmail.com>
2023-09-28 17:52:40 -0400

    small change to visual handler

--- src/Components/VisualSlider/VisualSlider.js
+++ src/Components/VisualSlider/VisualSlider.js
@@ -1,10 +1,10 @@
 import './VisualSlider.css';
 import {ImgHolder, VidHolder} from '../index.js'
 
-function VisualSlider(){
+function VisualSlider(props){
     return(
         <div className='VisualContainer'>
-            <ImgHolder source="https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/06/luffy-from-one-piece-goku-from-dragon-ball-z-and-saitama-from-one-punch-man.jpg" alt="abc"/>
+            <ImgHolder source={props.source} alt={props.alt}/>
         </div>
     )
 }
